c2c84e0627f919054927806f1fe689bbc9a1c1bc
free001style <iadrobyshevskiy@edu.hse.ru>
2024-10-14 23:44:20 +0300

    add collate fn

--- src/datasets/collate.py
+++ src/datasets/collate.py
@@ -13,13 +13,31 @@ def collate_fn(dataset_items: list[dict]):
         result_batch (dict[Tensor]): dict, containing batch-version
             of the tensors.
     """
-
-    result_batch = {}
-
-    # example of collate_fn
-    result_batch["data_object"] = torch.vstack(
-        [elem["data_object"] for elem in dataset_items]
-    )
-    result_batch["labels"] = torch.tensor([elem["labels"] for elem in dataset_items])
+    """instance_data = {
+            "mix": mix,
+            "label1": label1,
+            "label2": label2,
+            "mouths1": mouths1,
+            "mouths2": mouths2,
+            "mix_path": data_dict["mix"],
+            "label1_path": data_dict["label1"],
+            "label2_path": data_dict["label2"],
+        }"""
+    batch_size = len(dataset_items)
+    time_audio = dataset_items[0]["mix"].shape[0]
+    time_video, h, w = dataset_items[0]["mouths1"].shape
+    result_batch = {
+        "mix": torch.zeros((batch_size, time_audio)),
+        "label1": torch.zeros((batch_size, time_audio)),
+        "label2": torch.zeros((batch_size, time_audio)),
+        "mouths1": torch.zeros((batch_size, time_video, h, w)),
+        "mouths2": torch.zeros((batch_size, time_video, h, w)),
+        "label1_path": [""] * batch_size,
+        "label2_path": [""] * batch_size,
+    }
+    for i in range(batch_size):
+        for key in result_batch.keys():
+            # print(dataset_items[i][key].shape)
+            result_batch[key][i] = dataset_items[i][key]
 
     return result_batch

82168d24b4857f2ec5985c842f1f97f16be216a3
free001style <iadrobyshevskiy@edu.hse.ru>
2024-10-19 12:07:11 +0300

    update requirements and collate fn

--- src/datasets/collate.py
+++ src/datasets/collate.py
@@ -18,14 +18,17 @@ def collate_fn(dataset_items: list[dict]):
     time_video, h, w = dataset_items[0]["mouths1"].shape
     result_batch = {
         "mix": torch.zeros((batch_size, time_audio)),
-        "label1": torch.zeros((batch_size, time_audio)),
-        "label2": torch.zeros((batch_size, time_audio)),
-        "mouths1": torch.zeros((batch_size, time_video, h, w)),
-        "mouths2": torch.zeros((batch_size, time_video, h, w)),
-        "label1_path": [""] * batch_size,
-        "label2_path": [""] * batch_size,
+        "source": torch.zeros((batch_size, 2, time_audio)),
+        "video": torch.zeros((batch_size, 2, time_video, h, w)),
+        "source1_path": [""] * batch_size,
+        "source2_path": [""] * batch_size,
     }
     for i in range(batch_size):
-        for key in result_batch.keys():
-            result_batch[key][i] = dataset_items[i][key]
+        result_batch["mix"][i] = dataset_items[i]["mix"]
+        result_batch["source"][i][0] = dataset_items[i]["label1"]
+        result_batch["source"][i][1] = dataset_items[i]["label2"]
+        result_batch["video"][i][0] = dataset_items[i]["mouths1"]
+        result_batch["video"][i][1] = dataset_items[i]["mouths2"]
+        result_batch["source1_path"][i] = dataset_items[i]["label1_path"]
+        result_batch["source2_path"][i] = dataset_items[i]["label2_path"]
     return result_batch
